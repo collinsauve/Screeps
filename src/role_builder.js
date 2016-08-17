3a6bae3a228b5407b026c85d973be8cb80379b62
Collin Sauve <collinsauve@gmail.com>
2016-08-17 01:21:13 -0400

    still trying to fix builder

--- src/role_builder.js
+++ src/role_builder.js
@@ -1,17 +1,24 @@
 module.exports = (function () {
+
+    const log = require('logger')('role_builder');
     return {
         name: 'builder',
         body: [WORK, CARRY, CARRY, MOVE, MOVE],
         execution: function builder(creep) {		
-            if(creep.energy === 0) {
+
+            if (creep.carry[RESOURCE_ENERGY] === 0) {
+                log.debug('getting energy');
                 creep.moveTo(Game.spawns.Spawn1);
-                Game.spawns.Spawn1.transferEnergy(creep);
+                creep.withdraw(creep, RESOURCE_ENERGY);
             }
             else {
-                var targets = creep.room.find(Game.CONSTRUCTION_SITES);
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if(targets.length) {
+                    log.debug('going to build construction site');
                     creep.moveTo(targets[0]);
                     creep.build(targets[0]);
+                } else {
+                    log.debug('no construction site found');
                 }
             }
         }
