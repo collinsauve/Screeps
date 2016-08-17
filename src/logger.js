bdea092193ec8bd348c45475a3cc4f682e03a7d0
Collin Sauve <collinsauve@gmail.com>
2016-08-16 21:05:34 -0400

    allow files to override the minimum log level

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,19 @@
 module.exports = (function () {
 
-    return (category) => {
+    return (category, minimumLevel) => {
         const levels = {
             error: 0,
             warning: 1,
             info: 2,
             debug: 3,
         };
-        const minimumLevel = 1;
+        const levelAbbrev = ['E', 'W', 'I', 'D'];
+        const defaultMinimumLevel = levels.info;
+        if (minimumLevel === undefined) minimumLevel = defaultMinimumLevel;
 
         function log(level, message) {
             if (level <= minimumLevel) {
-                console.log(category + " - " + message);
+                console.log(levelAbbrev + ':' + category + ": " + message);
             }
         }
 
